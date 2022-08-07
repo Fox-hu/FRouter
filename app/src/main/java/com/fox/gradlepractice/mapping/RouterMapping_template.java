1120ac744836587950dc61c58e091a03a6056e86
hujia <hujia0523@gmail.com>
2022-08-07 22:11:58 +0800

    add function for create routermapping.java

---
+++ app/src/main/java/com/fox/gradlepractice/mapping/RouterMapping_template.java
@@ -0,0 +1,18 @@
+package com.fox.gradlepractice.mapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author Silver-Fox
+ * @Date 2022/8/7 21:17
+ * 模板 aop 中生成的类参照此类写
+ */
+class RouterMapping_template {
+    public static Map<String, String> get() {
+        HashMap<String, String> mapping = new HashMap<>();
+        mapping.put("router://","com.xxx.Activity");
+
+        return mapping;
+    }
+}
--- router-processor/src/main/java/com.fox.router.processor/DestinationProcessor.java
+++ router-processor/src/main/java/com.fox.router.processor/DestinationProcessor.java
@@ -2,7 +2,11 @@ package com.fox.router.processor;
 
 import com.fox.router.annotations.Destination;
 import com.google.auto.service.AutoService;
+
+import java.io.Writer;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -13,8 +17,10 @@ import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.tools.JavaFileObject;
 
 //一定要public class 否则报错
+//使用命令 ./gradlew :app:assembleDebug
 @AutoService(Processor.class)
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class DestinationProcessor extends AbstractProcessor {
@@ -46,12 +52,26 @@ public class DestinationProcessor extends AbstractProcessor {
             return false;
         }
 
+        //自动生成类的类名
+        String className = "RouterMapping_" + System.currentTimeMillis();
+        //生成类时 先自己写一个java类 参照写的类生成
+        //参照RouterMapping_template类
+        StringBuilder builder = new StringBuilder();
+        builder.append("package com.fox.gradlepractice.mapping;\n\n");
+        builder.append("import java.util.HashMap;\n");
+        builder.append("import java.util.Map;\n");
+        builder.append("public class ").append(className).append("{ \n\n");
+        builder.append("   public static Map<String, String> get() {  \n");
+        builder.append("      HashMap<String, String> mapping = new HashMap<>();\n");
+
         //遍历所有@Destination 注解信息 依次获取详细信息
         for (Element element : allElements) {
             //尝试在当前类中获取注解信息
             final TypeElement typeElement = (TypeElement) element;
             Destination destination = typeElement.getAnnotation(Destination.class);
-            if(destination == null) continue;
+            if (destination == null) {
+                continue;
+            }
 
             //非空 开始解析
             String url = destination.url();
@@ -59,7 +79,27 @@ public class DestinationProcessor extends AbstractProcessor {
             //拿到当前类的全路径
             String realPath = typeElement.getQualifiedName().toString();
 
-            logger.info(">>> url = " + url + " description = " + description + " realPath = " + realPath);
+            logger.info(">>> url = " + url + " description = " + description + " realPath = " +
+                        realPath);
+            builder.append("      ").append("mapping.put(").append("\"" + url + "\"").append(",")
+                    .append("\"" + realPath + "\"").append(");\n");
+        }
+
+        builder.append("      return mapping;\n");
+        builder.append("   }\n}\n");
+
+        String mappingFullClassName = "com.fox.gradlepractice.mapping." + className;
+        logger.info(">>> mappingFullClassName = " + mappingFullClassName);
+        logger.info(">>> class content = \n " + builder);
+        //写入java文件
+        try {
+            JavaFileObject source = processingEnv.getFiler().createSourceFile(mappingFullClassName);
+            Writer writer = source.openWriter();
+            writer.write(builder.toString());
+            writer.flush();
+            writer.close();
+        } catch (Exception ex) {
+            throw new RuntimeException("Error while create file ", ex);
         }
         logger.info(">>> processor end");
         return false;
