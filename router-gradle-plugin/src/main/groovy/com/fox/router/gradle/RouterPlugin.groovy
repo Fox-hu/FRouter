64cfb7ddb0c6144f1df0370bd9b77b6d2ee6efce
hujia <hujia@bilibili.com>
2022-08-08 19:15:53 +0800

    add create mk、delete router_mapping dir function

--- router-gradle-plugin/src/main/groovy/com/fox/router/gradle/RouterPlugin.groovy
+++ router-gradle-plugin/src/main/groovy/com/fox/router/gradle/RouterPlugin.groovy
@@ -1,5 +1,6 @@
 package com.fox.router.gradle
 
+import groovy.json.JsonSlurper
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -12,28 +13,80 @@ class RouterPlugin implements Plugin<Project> {
         //2.实现旧构建产物的自动清理
         //3.在javac任务后 汇总生成文档
         /**
-         *     kapt {
-         *         arguments {
-         *             arg("root_project_dir", rootProject.projectDir.absolutePath)
-         *         }
-         *     }
-         */
+         *     kapt {          arguments {              arg("root_project_dir", rootProject.projectDir.absolutePath)
+         *}}*/
 
         println("get root project dir")
+        //1.帮用户传递路径参数到注解处理器中
         if (project.extensions.findByName("kapt") != null) {
             project.extensions.findByName("kapt").arguments {
-                arg("root_project_dir",project.getRootProject().projectDir.absolutePath)
+                arg("root_project_dir", project.getRootProject().projectDir.absolutePath)
+            }
+        }
+
+        //2.实现旧构建产物的自动清理
+        project.clean.doFirst {
+            //删除上一次构建生成的 router_mapping目录
+            File routerMappingDir = new File(project.rootProject.projectDir, "router_mapping")
+            if (routerMappingDir.exists()) {
+                println("delete dir = ${routerMappingDir.absolutePath}")
+                routerMappingDir.deleteDir()
             }
         }
 
         println("from RouterPlugin, apply from ${project.name}")
         //注册extension
         project.getExtensions().create("router", RouterExtension)
-
         project.afterEvaluate {
             //获取用户配置的插件参数
             RouterExtension extension = project["router"]
             println("setting dir = ${extension.wikiDir}")
+
+            //3.在javac任务后(compileDebugJavaWithJavac) 汇总生成文档
+            project.tasks.findAll {
+                it.name.startsWith('compile') && it.name.endsWith('JavaWithJavac')
+            }.each {
+                it.doLast {
+                    File routerMappingDir = new File(project.rootProject.projectDir, "router_mapping")
+                    if (!routerMappingDir.exists()) {
+                        return
+                    }
+                    File[] allChildFiles = routerMappingDir.listFiles()
+                    if (allChildFiles.length < 1) {
+                        return
+                    }
+
+                    StringBuilder markdownBuilder = new StringBuilder()
+                    markdownBuilder.append("# 页面文档\n\n")
+                    allChildFiles.each { child ->
+                        if (child.name.endsWith(".json")) {
+                            JsonSlurper jsonSlurper = new JsonSlurper()
+                            def content = jsonSlurper.parse(child)
+                            content.each { innerContent ->
+
+                                def url = innerContent['url']
+                                def description = innerContent['description']
+                                def realPath = innerContent['realPath']
+
+                                markdownBuilder.append("## $description \n")
+                                markdownBuilder.append("- url: $url \n")
+                                markdownBuilder.append("- realPath: $realPath \n\n")
+                            }
+                        }
+                    }
+
+                    File wikiFileDir = new File(extension.wikiDir)
+                    if (!wikiFileDir.exists()) {
+                        wikiFileDir.mkdir()
+                    }
+
+                    File wikiFile = new File(wikiFileDir, "页面文档.md")
+                    if (wikiFile.exists()) {
+                        wikiFile.delete()
+                    }
+                    wikiFile.write(markdownBuilder.toString())
+                }
+            }
         }
     }
 }
